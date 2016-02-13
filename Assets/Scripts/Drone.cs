f8b6f9d536d5b5d222f42a431e308c9a0b5ba693
dvdb2 <2d.vandenberg@gmail.com>
2016-02-13 20:35:32 +0100

    UPdate robot and damage script

--- Assets/Scripts/Drone.cs
+++ Assets/Scripts/Drone.cs
@@ -7,7 +7,7 @@ public class Drone : BaseTargetBehaviour
     public override void ReceiveHit(RaycastHit hit)
     {
         //transform.DOPunchScale(Vector3.one * 0.2f, 2f);
-        GetComponent<Weapon_Health>().setDamage(10f);
+        GetComponent<HealthManager>().setDamage(10f);
     }
 
     public override TargetType GetTargetType()
