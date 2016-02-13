bc9ebaa2b315d7c756d31e0c6adf2beabd2be95b
dvdb2 <2d.vandenberg@gmail.com>
2016-02-13 17:01:42 +0100

    udate

--- Assets/Scripts/Enemy_WeaponSystem.cs
+++ Assets/Scripts/Enemy_WeaponSystem.cs
@@ -3,8 +3,13 @@ using System.Collections;
 
 public class Enemy_WeaponSystem : MonoBehaviour {
 
-    public GameObject[] weapons;
+    public GameObject weapon;
     public GameObject fireHartpoint;
+    public float fireInterval = 5f;
+    public float burstInterval = 0.3f;
+    public bool enableBurstMode = true;
+    public int photonSalvo = 5;
+
     //([Header],"s");
     public float weaponRotationOffset = 0f;
     public float weaponSpeed = 2000f;
@@ -14,6 +19,7 @@ public class Enemy_WeaponSystem : MonoBehaviour {
 
     //do not change these vales
     private float cooldown;
+    private float shootTime1 = 0.0f;
 
 
     // Use this for initialization
@@ -25,14 +31,63 @@ public class Enemy_WeaponSystem : MonoBehaviour {
 	void Update ()
     {
         //CheckForFireing();
+        FireSelectedWeapon();
     }
 
-    IEnumerator FireWeapon()
+    void FireSelectedWeapon()
     {
-        while (enableFireing)
+        if (Time.time >= shootTime1)
         {
 
-            yield return new WaitForSeconds(textInterval);
+            if (fireHartpoint != null)
+            {
+                if (enableBurstMode)
+                {
+                    StartCoroutine(FireWeaponObjectBurst());
+                }
+                else
+                {
+                    FireWeaponObjectNormal();
+                }
+            }
+            else
+            {
+                Debug.Log("[" + this.name + "] " + "Please select a firehartpoint first");
+            }
+
+            shootTime1 = Time.time + fireInterval;
         }
     }
+
+    // [A] creates a new instance of the beam or projectyle weapon using Burst fire
+    IEnumerator FireWeaponObjectBurst()
+    {
+        Debug.Log(" fire in burstMode.");
+
+        // fire x photons per salvo shot
+        for (int i = 0; i < photonSalvo; i++)
+        {
+            // Create a new instance of weapon[currentSeclected], startfrom firehartpoint position, and use firehartpoint Y rotation to [x.y.z])
+            weapon = (GameObject)Instantiate(weapon, fireHartpoint.transform.position, Quaternion.Euler(0, fireHartpoint.transform.eulerAngles.y + weaponRotationOffset, 0));
+
+            // Fire weapon at speed x
+            weapon.GetComponent<Rigidbody>().AddForce(transform.forward * weaponSpeed);
+
+            yield return new WaitForSeconds(burstInterval);
+        }
+    }
+
+
+    // [B] creates a new instance of the beam or projectyle weapon
+    void FireWeaponObjectNormal()
+    {
+        Debug.Log(" fire in normalMode.");
+
+        // create a new instance of weapon[currentSeclected], starfrom firehartpoint position, and use firehartpoint Y rotation to [x.y.z])
+        weapon = (GameObject)Instantiate(weapon, fireHartpoint.transform.position, Quaternion.Euler(0, fireHartpoint.transform.eulerAngles.y + weaponRotationOffset, 0));
+
+        // Fire weapon at speed x
+        weapon.GetComponent<Rigidbody>().AddForce(transform.forward * weaponSpeed);
+    }
+
 }
--- Assets/Scripts/Weapon_Stats.cs
+++ Assets/Scripts/Weapon_Stats.cs
@@ -1,15 +1,111 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
-public class Weapon_Stats : MonoBehaviour {
+public class Weapon_Stats : MonoBehaviour 
+{
+    //private enum WeaponTypes {Main, Options, Extras};
+    public LayerMask hitMask;
+    //public GameObject impactPrefab;
+    //public AudioClip impactEffect;
 
-	// Use this for initialization
-	void Start () {
-	
+    //private ParticleSystem impactParticle;
+
+    //void Awake()
+    //{
+    //    ////audioFile = explosionParticle.GetComponent<AudioSource>();
+    //    impactPrefab = Instantiate(this.impactPrefab);
+    //    //impactParticle = impactPrefab.GetComponentInChildren<ParticleSystem>();
+    //    //impactPrefab.SetActive(false);
+
+    //    if (impactPrefab)
+    //    {
+    //        impactParticle = impactPrefab.GetComponentInChildren<ParticleSystem>();
+    //        impactPrefab.SetActive(false);
+    //    }
+    //    else
+    //    {
+    //        Debug.Log("[Weapon_Stat] No Effect frefab for  weapon seleceted");
+    //    }
+    //}
+
+
+    /// <summary>
+    /// The name of the weapon.
+    /// </summary>
+    public string _weaponName = "";
+	public string WeaponName
+	{
+		get{return _weaponName;}
+		set{_weaponName = value;}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	/// <summary>
+	/// The weapon description.
+	/// </summary>
+	public string _weaponDescription = "";
+	public string WeaponDescription
+	{
+		get{return _weaponDescription;}
+		set{_weaponDescription = value;}
 	}
+
+
+	/// <summary>
+	/// The weapon types.
+	/// </summary>
+	public WeaponTypes _weaponTypes;
+	public enum WeaponTypes
+	{
+		Projectile,
+		DoTProjectile,
+	}   
+
+	/// <summary>
+	/// The weapon damage on impact or on DOT
+	/// </summary>
+	public float _weaponDamage = 1f;
+	public float WeaponDamage
+	{
+		get{return _weaponDamage;}
+		set{_weaponDamage = value;}
+	}
+
+	/// <summary>
+	/// The weapon Damage Over Time (D.O.T) time.
+	/// </summary>
+	public float _weaponDOTTime = 1f;
+	public float WeaponDOTTime
+	{
+		get{return _weaponDOTTime;}
+		set{_weaponDOTTime = value;}
+	}
+
+	/// <summary>
+	/// The weapon DOT interval. default once per second
+	/// </summary>
+	public float _weaponDOTInterval = 1f;
+	public float WeaponDOTInterval
+	{
+		get{return _weaponDOTInterval;}
+		set{_weaponDOTInterval = value;}
+	}
+
+    void OnTriggerEnter(Collider collidedObject)
+    {
+        // Debug.Log("Detecting hit: objectname: " + colidedObject.name + " On layer: " + colidedObject.gameObject.layer);
+        float layerID = Mathf.Log(hitMask.value, 2);
+
+        // Debug.Log("comparing: " + colidedObject.gameObject.layer + " to " + layerID);
+        if (collidedObject.gameObject.layer == layerID)
+        {
+            Debug.Log("i hit: " + collidedObject.name);
+            //if (collidedObject.gameObject.GetComponentInParent<Player_CarHealth>())
+            //{
+            //    Debug.Log("Found HeatlhSystem");
+            //    collidedObject.gameObject.GetComponentInParent<Player_CarHealth>().setDamage(WeaponDamage);
+            //}
+        }
+        Destroy(this.gameObject);
+    }  
 }
