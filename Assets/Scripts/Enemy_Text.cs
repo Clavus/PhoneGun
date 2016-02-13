166ee646614298c0074e42bca2bffc308ce9ce13
dvdb2 <2d.vandenberg@gmail.com>
2016-02-13 16:14:03 +0100

    Update enemy

---
+++ Assets/Scripts/Enemy_Text.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+
+public class Enemy_Text : MonoBehaviour {
+
+    public string[] textMessages;
+    public bool showTexts;
+    public float textInterval = 5f;
+
+
+    public Text enemyText;
+
+    // Use this for initialization
+    void Start () {
+
+        if (showTexts)
+        {
+            StartCoroutine(ShowTexts());
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    IEnumerator ShowTexts()
+    {
+
+        foreach(string message in textMessages)
+        {
+            //enemyText.GetComponent<Text>().text = message;
+            enemyText.text = message;
+            yield return new WaitForSeconds(textInterval);
+        }
+        
+    }
+
+
+}
---
+++ Assets/Scripts/Enemy_WeaponSystem.cs
@@ -0,0 +1,38 @@
+﻿ using UnityEngine;
+using System.Collections;
+
+public class Enemy_WeaponSystem : MonoBehaviour {
+
+    public GameObject[] weapons;
+    public GameObject fireHartpoint;
+    //([Header],"s");
+    public float weaponRotationOffset = 0f;
+    public float weaponSpeed = 2000f;
+    public AudioClip fireSound;
+    public bool enableFireing = true;
+
+
+    //do not change these vales
+    private float cooldown;
+
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //CheckForFireing();
+    }
+
+    IEnumerator FireWeapon()
+    {
+        while (enableFireing)
+        {
+
+            yield return new WaitForSeconds(textInterval);
+        }
+    }
+}
