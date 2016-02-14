40e25a4e6220e006bd5c87975b013b0855574c0d
dvdb2 <2d.vandenberg@gmail.com>
2016-02-14 15:13:20 +0100

    update drone damage

--- Assets/Scripts/Drone.cs
+++ Assets/Scripts/Drone.cs
@@ -4,10 +4,11 @@ using DG.Tweening;
 
 public class Drone : BaseTargetBehaviour
 {
+    public float setDamage = 34f;
     public override void ReceiveHit(RaycastHit hit)
     {
         //transform.DOPunchScale(Vector3.one * 0.2f, 2f);
-        GetComponent<HealthManager>().setDamage(10f);
+        GetComponent<HealthManager>().setDamage(setDamage);
     }
 
     public override TargetType GetTargetType()
